2fe33384d7ee9b16a4974164f346f8195b1097ce
JuanCabelloVargas <jicabello@miuandes.cl>
2025-09-12 20:56:27 -0300

    feat: Create index route for guests

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get "/rooms", to: "rooms#index"
   get "/rooms/:id", to: "rooms#show", as: "room"
+
+  get "/guests", to: "guests#index"
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
